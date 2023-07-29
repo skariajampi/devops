0af8a7ceb47626982a1d89de77c2d10892e9b1d2
Skaria <skariajampi@gmail.com>
2023-07-29 09:52:53 +0100

    initial commit

---
+++ start_docker.sh
@@ -0,0 +1,4 @@
+docker-compose -f docker/nexus/nexus-docker-compose.yml up -d 
+
+docker-compose -f docker/sonarqube/sonarqube-docker-compose.yml up -d 
+
---
+++ stop_docker.sh
@@ -0,0 +1,4 @@
+docker-compose -f docker/nexus/nexus-docker-compose.yml down  
+
+docker-compose -f docker/sonarqube/sonarqube-docker-compose.yml down 
+
