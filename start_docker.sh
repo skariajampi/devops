8892810edb633068b25ff81b67fb7f7a994e1415
Skaria <skariajampi@gmail.com>
2023-07-30 19:07:49 +0100

    added ansible scripts to start and stop nexus and sonarqube

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker/nexus/nexus-docker-compose.yml up -d 
+docker-compose -f nexus-docker-compose.yml up -d
 
-docker-compose -f docker/sonarqube/sonarqube-docker-compose.yml up -d 
+docker-compose -f sonarqube-docker-compose.yml up -d
 
--- stop_docker.sh
+++ stop_docker.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker/nexus/nexus-docker-compose.yml down  
+docker-compose -f nexus-docker-compose.yml down
 
-docker-compose -f docker/sonarqube/sonarqube-docker-compose.yml down 
+docker-compose -f sonarqube-docker-compose.yml down
 
